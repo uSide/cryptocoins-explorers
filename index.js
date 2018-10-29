02b7df8fa7305505c131d0ac8cc6e672177a48b5
Vladimir Kleshko <uside.node@gmail.com>
2018-10-29 14:14:32 +0200

    Add formatter

---
+++ index.js
@@ -0,0 +1,22 @@
+let explorers = {
+  mainnet: {
+    btc: 'https://blockchain.info/tx/{{hash}}',
+    ltc: 'http://explorer.litecoin.net/tx/{{hash}}',
+    bch: 'https://explorer.bitcoin.com/bch/tx/{{hash}}',
+    xmr: 'http://moneroblocks.info/tx/{{hash}}',
+    dash: 'https://chainz.cryptoid.info/dash/tx.dws?{{hash}}.htm',
+    eth: 'https://etherscan.io/tx/{{hash}}'
+  },
+  testnet: {
+    btc: 'https://live.blockcypher.com/btc-testnet/tx/{{hash}}/',
+    ltc: 'https://chain.so/tx/LTCTEST/{{hash}}',
+    bch: 'https://www.blocktrail.com/tBCC/tx/{{hash}}',
+    xmr: 'https://testnet.xmrchain.net/tx/{{hash}}',
+    dash: 'https://chain.so/tx/DASHTEST/{{hash}}',
+    eth: 'https://kovan.etherscan.io/tx/{{hash}}'
+  }
+}
+
+module.exports = (net, coin, hash) => {
+  return explorers[net][coin.toLowerCase()].replace('{{hash}}', hash)
+}
