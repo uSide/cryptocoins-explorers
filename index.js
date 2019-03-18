7e59f1b6a11a0fb74ad49d1685f51cad9e05e470
Vladimir Kleshko <uside.node@gmail.com>
2019-03-18 20:42:14 +0200

    Update
    - Add Tether
    - Add 0x
    - Add True USD
    - Add USD Coin

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ let explorers = {
     bch: 'https://explorer.bitcoin.com/bch/tx/{{hash}}',
     xmr: 'http://moneroblocks.info/tx/{{hash}}',
     dash: 'https://chainz.cryptoid.info/dash/tx.dws?{{hash}}.htm',
-    eth: 'https://etherscan.io/tx/{{hash}}',
     xrp: 'https://xrpcharts.ripple.com/#/transactions/{{hash}}',
     xlm: 'https://stellarchain.io/tx/{{hash}}',
     ada: 'https://cardanoexplorer.com/tx/{{hash}}',
     trx: 'https://tronscan.org/#/transaction/{{hash}}',
     miota: 'https://thetangle.org/transaction/{{hash}}',
-    zec: 'https://chain.so/tx/ZEC/{{hash}}'
+    zec: 'https://chain.so/tx/ZEC/{{hash}}',
+    usdt: 'https://omniexplorer.info/tx/{{hash}}',
+    eth: 'https://etherscan.io/tx/{{hash}}',
+    zrx: 'https://etherscan.io/tx/{{hash}}',
+    tusd: 'https://etherscan.io/tx/{{hash}}',
+    usdc: 'https://etherscan.io/tx/{{hash}}',
   },
   testnet: {
     btc: 'https://live.blockcypher.com/btc-testnet/tx/{{hash}}/',
@@ -19,16 +23,18 @@ let explorers = {
     bch: 'https://www.blocktrail.com/tBCC/tx/{{hash}}',
     xmr: 'https://testnet.xmrchain.net/tx/{{hash}}',
     dash: 'https://chain.so/tx/DASHTEST/{{hash}}',
-    eth: 'https://kovan.etherscan.io/tx/{{hash}}',
     xrp: 'https://ripplerm.github.io/ripple-wallet/',
     xlm: 'https://testnet.steexp.com/tx/{{hash}}',
     ada: 'http://cardano-explorer.cardano-testnet.iohkdev.io/tx/{{hash}}',
     trx: 'https://shasta.tronscan.org/#/transaction/{{hash}}',
     miota: 'https://devnet.thetangle.org/transaction/{{hash}}',
-    zec: 'https://explorer.testnet.z.cash/tx/{{hash}}'
+    zec: 'https://explorer.testnet.z.cash/tx/{{hash}}',
+    eth: 'https://ropsten.etherscan.io/tx/{{hash}}',
+    tst: 'https://etherscan.io/tx/{{hash}}',
   }
 }
 
 module.exports = (net, coin, hash) => {
-  return explorers[net][coin.toLowerCase()].replace('{{hash}}', hash)
+  let template = explorers[net][coin.toLowerCase()]
+  return (template || '{{hash}}').replace('{{hash}}', hash)
 }
